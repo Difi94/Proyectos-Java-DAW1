dad54a5fdf54bb07e4ac6c03e8218471265386bf
Juan Carlos <jloppon943@g.educaand.es>
2025-12-01 20:52:26 +0100

    continuacion ejercicios con objetos 1/12/25

--- LopezJuanCarlos5.16.UtilizandoObjetos/src/Ejercicio1/Ejercicio1.java
+++ LopezJuanCarlos5.16.UtilizandoObjetos/src/Ejercicio1/Ejercicio1.java
@@ -1,5 +1,7 @@
 package Ejercicio1;
 
+import java.time.DateTimeException;
+
 import libtarea3.CuentaBancaria;
 
 public class Ejercicio1 {
@@ -13,6 +15,35 @@ public class Ejercicio1 {
 		cuentaConjunta = new CuentaBancaria();
 		cuentaFamiliar = new CuentaBancaria();
 		
+		try {
+		    CuentaBancaria pruebaError = new CuentaBancaria(
+		        2000,
+		        java.time.LocalDate.of(2020, 9, 1)); //si ponemos 2027 en la fecha saltará el error correspondiente
+		    CuentaBancaria pruebaError1 = new CuentaBancaria(2000); //si ponemos saldo negatio saldrá su error correspondiente
+		    cuentaPrivada = new CuentaBancaria(1000, java.time.LocalDate.of(2021, 7, 1), -200);
+		    cuentaConjunta = new CuentaBancaria(200, java.time.LocalDate.of(2021, 7, 1));
+		    cuentaFamiliar = new CuentaBancaria();
+		    
+		} catch (DateTimeException ex1) {
+		    System.out.println("Fecha inválida: " + ex1.getMessage());
+		} catch (IllegalArgumentException ex2) {
+			System.out.println(ex2.getMessage());
+		}
+
+	    System.out.println("ID de la cuenta Privada: " + cuentaPrivada.getId());
+	    System.out.println("Fecha de creación: " + cuentaPrivada.getFechaCreacion());
+	    System.out.printf("Limite descubierto: %.2f\n", cuentaPrivada.getLimiteDescubierto());
+	    System.out.println("Está la cuenta embargada?: " + cuentaPrivada.isEmbargada());
+	    System.out.println("Está la cuenta en descubierto?: " + cuentaPrivada.isDescubierta());
+	    System.out.println("Número de días que lleva la cuenta abierta: " + cuentaPrivada.getDiasCuenta());
+	    
+	    cuentaPrivada.ingresar(100);
+	    cuentaConjunta.extraer(100);
+	    cuentaPrivada.transferir(100, cuentaFamiliar);
+	    
+	    System.out.println(cuentaPrivada.toString());
+	    System.out.println(cuentaConjunta.toString());
+	    System.out.println(cuentaFamiliar.toString());
 
 	}
 
--- LopezJuanCarlos5.16.UtilizandoObjetos/src/Ejercicio2/Ejercicio2.java
+++ LopezJuanCarlos5.16.UtilizandoObjetos/src/Ejercicio2/Ejercicio2.java
@@ -1,9 +1,23 @@
 package Ejercicio2;
 
+import libtarea3.Dado;
+
 public class Ejercicio2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Dado Ejercicio02 = new Dado();
+		System.out.printf("Número total de dados creados: %d "
+				+ "\nNúmero total de lanzamientos: %d"
+				+ " \nCantidad de veces que ha salido la cara 1 %d"
+				+ " \nCantidad de veces que ha salido la cara 2 %d"
+				+ " \nCantidad de veces que ha salido la cara 3 %d"
+				+ " \nCantidad de veces que ha salido la cara 4 %d",
+				Dado.getNumeroDadosCreados(),
+				Dado.getNumeroLanzamientosGlobal(),
+				Dado.getNumeroVecesCaraGlobal(1),
+				Dado.getNumeroVecesCaraGlobal(2),
+				Dado.getNumeroVecesCaraGlobal(3),
+				Dado.getNumeroVecesCaraGlobal(4));
 
 	}
 
