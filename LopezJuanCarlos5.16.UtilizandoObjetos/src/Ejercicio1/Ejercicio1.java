47616e6a0a525114c0dbf08d325dff928c24841d
Juan Carlos <jloppon943@g.educaand.es>
2025-12-03 21:26:29 +0100

    ejercicio dado y correccion apartado 1

--- LopezJuanCarlos5.16.UtilizandoObjetos/src/Ejercicio1/Ejercicio1.java
+++ LopezJuanCarlos5.16.UtilizandoObjetos/src/Ejercicio1/Ejercicio1.java
@@ -7,29 +7,54 @@ import libtarea3.CuentaBancaria;
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
+		//1
 		CuentaBancaria cuentaPrivada;
 		CuentaBancaria cuentaConjunta;
 		CuentaBancaria cuentaFamiliar;
+		
 		
 		cuentaPrivada = new CuentaBancaria();
 		cuentaConjunta = new CuentaBancaria();
 		cuentaFamiliar = new CuentaBancaria();
 		
+		//2.1
 		try {
 		    CuentaBancaria pruebaError = new CuentaBancaria(
 		        2000,
-		        java.time.LocalDate.of(2020, 9, 1)); //si ponemos 2027 en la fecha saltará el error correspondiente
-		    CuentaBancaria pruebaError1 = new CuentaBancaria(2000); //si ponemos saldo negatio saldrá su error correspondiente
-		    cuentaPrivada = new CuentaBancaria(1000, java.time.LocalDate.of(2021, 7, 1), -200);
-		    cuentaConjunta = new CuentaBancaria(200, java.time.LocalDate.of(2021, 7, 1));
-		    cuentaFamiliar = new CuentaBancaria();
+		        java.time.LocalDate.of(2027, 9, 1));
+		    //2.2
+		    CuentaBancaria pruebaError1 = new CuentaBancaria(-2000);
 		    
 		} catch (DateTimeException ex1) {
 		    System.out.println("Fecha inválida: " + ex1.getMessage());
 		} catch (IllegalArgumentException ex2) {
 			System.out.println(ex2.getMessage());
 		}
-
+		
+		//2.3
+		try {
+		    cuentaPrivada = new CuentaBancaria(1000, java.time.LocalDate.of(2021, 7, 1), -200);
+		    
+		} catch (IllegalArgumentException ex2) {
+			System.out.println(ex2.getMessage());
+		} catch (Exception ex21) {
+		    System.out.println("Error desconocido: " + ex21.getMessage());
+		    cuentaPrivada = new CuentaBancaria();
+		}
+		//2.4
+		try {
+		    cuentaConjunta = new CuentaBancaria(200, java.time.LocalDate.of(2021, 7, 1));
+		    
+		} catch (IllegalArgumentException ex2) {
+			System.out.println(ex2.getMessage());
+		} catch (Exception ex21) {
+		    System.out.println("Error desconocido: " + ex21.getMessage());
+		    cuentaConjunta = new CuentaBancaria();
+		}
+		//2.5
+		cuentaFamiliar = new CuentaBancaria();
+		
+		//3
 	    System.out.println("ID de la cuenta Privada: " + cuentaPrivada.getId());
 	    System.out.println("Fecha de creación: " + cuentaPrivada.getFechaCreacion());
 	    System.out.printf("Limite descubierto: %.2f\n", cuentaPrivada.getLimiteDescubierto());
@@ -37,10 +62,37 @@ public class Ejercicio1 {
 	    System.out.println("Está la cuenta en descubierto?: " + cuentaPrivada.isDescubierta());
 	    System.out.println("Número de días que lleva la cuenta abierta: " + cuentaPrivada.getDiasCuenta());
 	    
-	    cuentaPrivada.ingresar(100);
-	    cuentaConjunta.extraer(100);
-	    cuentaPrivada.transferir(100, cuentaFamiliar);
+	    //4.1
+	    try {
+	    	cuentaPrivada.ingresar(100);
+	    } catch (IllegalArgumentException ex4) {
+	    	System.out.println(ex4.getMessage());
+	    } catch (IllegalStateException ex41) {
+	    	System.out.println(ex41.getMessage());
+	    	
+	    }
+	    //4.2
+	    try {
+		    cuentaConjunta.extraer(100);
+	    } catch (IllegalArgumentException ex4) {
+	    	System.out.println(ex4.getMessage());
+	    } catch (IllegalStateException ex41) {
+	    	System.out.println(ex41.getMessage());
+	    	
+	    }
 	    
+	    //4.3
+	    try {
+		    cuentaPrivada.transferir(100, cuentaFamiliar);
+	    } catch (IllegalArgumentException ex4) {
+	    	System.out.println(ex4.getMessage());
+	    } catch (IllegalStateException ex41) {
+	    	System.out.println(ex41.getMessage());
+	
+	    }
+	    
+	    
+	    //5
 	    System.out.println(cuentaPrivada.toString());
 	    System.out.println(cuentaConjunta.toString());
 	    System.out.println(cuentaFamiliar.toString());
--- LopezJuanCarlos5.16.UtilizandoObjetos/src/Ejercicio2/Ejercicio2.java
+++ LopezJuanCarlos5.16.UtilizandoObjetos/src/Ejercicio2/Ejercicio2.java
@@ -6,6 +6,7 @@ public class Ejercicio2 {
 
 	public static void main(String[] args) {
 		Dado Ejercicio02 = new Dado();
+		//1
 		System.out.printf("Número total de dados creados: %d "
 				+ "\nNúmero total de lanzamientos: %d"
 				+ " \nCantidad de veces que ha salido la cara 1 %d"
@@ -18,7 +19,15 @@ public class Ejercicio2 {
 				Dado.getNumeroVecesCaraGlobal(2),
 				Dado.getNumeroVecesCaraGlobal(3),
 				Dado.getNumeroVecesCaraGlobal(4));
-
+		
+		//2
+		for (int i = 0; i < 10; i++) {
+			try {
+				Dado CarasAleatorias = new Dado(Math.random(8));
+			} catch (IllegalArgumentException ex1) {
+				System.out.println(ex1.getMessage());
+			}
+		}
 	}
 
 }
